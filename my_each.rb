953a5dde5275dfa871d2a357db96ae820c60edd8
Amanda M <amandaseagull3@gmail.com>
2018-03-30 17:38:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -10,15 +10,6 @@ end
 collection
 end
 
-# def my_each(collection)
-#   i = 0
-#   while i < collection.length
-#     yield(collection[i])
-#     i = i + 1
-#   end
-#   collection
-# end
-
 
 # def hello_t(array)
 #   if block_given?
